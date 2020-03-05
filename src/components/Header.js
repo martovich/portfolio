a4180f0a18ce90fca0f4b2240f143f3255efadb5
martovich <mart-1984@mail.ru>
2020-03-06 01:43:56 +0300

    add containers

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from '../modules/Header.module.css';
+import NavMenu from "./NavMenu";
 
 const Header = () => (
     <div className={styles.header}>
+        <div className={styles.container}>
+            <NavMenu />
+        </div>
     </div>
 );
 export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,16 @@ import styles from '../modules/Main.module.css';
 
 const Main = () => (
     <div className={styles.main}>
-
+        <div className={styles.container}>
+            <div className={styles.greeating}>
+                <span>Привет!</span>
+                <span>Меня зовут...</span>
+                <span>Я Front-end...</span>
+            </div>
+            <div className={styles.photo}>
+                <img src="" alt=""/>
+            </div>
+        </div>
     </div>
 );
 export default Main;
