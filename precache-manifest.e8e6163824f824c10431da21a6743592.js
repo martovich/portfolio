03dcfd97c57b4d152f4b2bf8a62bebb6cf249828
martovich <mart-1984@mail.ru>
2020-03-11 01:23:32 +0300

    Updates

---
+++ precache-manifest.e8e6163824f824c10431da21a6743592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d1d79915cff1f60cbb0d057edfb3478",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3a9edd0794d053c6cc6e",
+    "url": "/portfolio/static/css/main.1bb91c31.chunk.css"
+  },
+  {
+    "revision": "e6a172ba5d5103084614",
+    "url": "/portfolio/static/js/2.b8750d74.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.b8750d74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9edd0794d053c6cc6e",
+    "url": "/portfolio/static/js/main.16f8ef3c.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  }
+]);
